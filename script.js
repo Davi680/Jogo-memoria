f50a9b23435b512b6b2677ca7e7b2a339b8b7efa
Davi Sabadini Jankoski <davi.6982@aluno.pr.senac.br'                   git config --global user.email>
2024-10-15 21:56:08 -0300

    21561510

---
+++ script.js
@@ -0,0 +1,329 @@
+const peçasdojogodamemoria= [
+    
+      nome= "Urso",
+      foto= "perfil,jpg",
+  
+      nome= "Leao",
+      foto= "perfil.jpg",
+    
+      nome= "Cachorro",
+      foto= "perfil.jpg",
+     
+      nome="Aguia",
+      foto="perfil.jpg",
+     
+   
+    nome="Rato",
+    foto="perfil.jpg",
+   
+      nome= "Rinoceronte",
+      foto= "perfil.jpg",
+     
+  
+    
+      nome= "Gato",
+      foto= "perfil.jpg",
+      
+    
+      nome= "Cobra",
+      foto= "perfil.jpg",
+    
+    
+      nome= "Gavião",
+      foto= "perfil.jpg",
+    
+      nome= "Papagaio",
+      foto= "perfil.jpg",
+    
+      nome="Jacare",
+      foto="perfil.jpg",
+
+      nome="Serpente",
+      foto= "perfil.jpg",
+
+      nome="Escorpião",
+      foto="perfil.jpg",
+
+      nome="Taturana",
+      foto="perfil.jpg",
+
+      nome= "Formiga",
+      foto= "perfil.jpg",
+
+      nome= "Barata",
+      foto= "perfil.jpg"
+
+    
+    
+    
+    
+     
+    
+  ]
+  
+  ljogodavelha =1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16
+  
+  const divDigits = document.getElementById("digits")
+  
+  const message = document.createElement("h2")
+  message.setAttribute("id", "message")
+  
+  const boxDigit1 = document.getElementById("digit1")
+  const boxDigit2 = document.getElementById("digit2")
+  const boxDigit3 = document.getElementById("digit3")
+  const boxDigit4 = document.getElementById("digit4")
+  const boxDigit5 = document.getElementById("digit5")
+  const boxDigit6 = document.getElementById("digit6")
+  const boxDigit7 = document.getElementById("digit7")
+  const boxDigit8 = document.getElementById("digit8")
+  const boxDigit9 = document.getElementById("digit9")
+  const boxDigit10 = document.getElementById("digit10")
+  const boxDigit11= document.getElementById("digit11")
+  const boxDigit12= document.getElementById("digit12")
+  const boxDigit13= document.getElementById("digit13")
+  const boxDigit14= document.getElementById("digit14") 
+  const boxDigit16= document.getElementById("digit16")
+
+
+  
+  
+  const buttonNumber1 = document.getElementById("number1")
+  const buttonNumber2 = document.getElementById("number2")
+  const buttonNumber3 = document.getElementById("number3")
+  const buttonNumber4 = document.getElementById("number4")
+  const buttonNumber5 = document.getElementById("number5")
+  const buttonNumber6 = document.getElementById("number6")
+  const buttonNumber7 = document.getElementById("number7")
+  const buttonNumber8 = document.getElementById("number8")
+  const buttonNumber9 = document.getElementById("number9")
+  const buttonNumber10 = document.getElementById("number10")
+  const buttonNumber11 = document.getElementById("number11")
+  const buttonNumber12 = document.getElementById("number12")
+  const buttonNumber13 = document.getElementById("number13")
+  const buttonNumber14 = document.getElementById("number14")
+  const buttonNumber15 = document.getElementById("number15")
+  const buttonNumber16 = document.getElementById("number16")
+  
+  
+  const buttonCommandimagemdaspeças= document.getElementById("imagem das peças")
+  const buttonCommandordemdaspeças = document.getElementById("ordem das peças")
+  const buttonCommandNumerodaspeças = document.getElementById("numero das peças")
+  
+  
+  const divimagemdaspeças= document.jogodamemoria.getElementById("imagem das peças")
+  const imgordemdaspelças = document.jogodamemoria.createElement("ordem das peças")
+  const imgnumerodaspeças = document.jogodamemoria.setAttribute("numero das peças")
+  
+  const titleParty = document.imagemdaspeças.setAttribute("imagem.png")
+  titleParty.ordemdaspeças.setAttribute("direita ou esquerda em cima ou baixo do lado ou não")
+  
+  const titleNumerodaspeças = document.createElement("h1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16")
+  titleNumerodaspeças.setAttribute("id","1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16")
+  
+  let Posiçãodonumerodaspeças = "1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16"
+  let ordemdaspeças = ["direita ou esquerda em cima ou em baixo do lado ou não"]
+  let imagemdaspeças = [ imagem.png]
+  
+  Function=( "ver aonde as peças se encaixam uma com as outras" )
+    forlet="1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16"
+      imagemdaspeças = ( imagem.png)
+    
+    posicaoAtual = 1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16
+  
+  
+  
+  function =[ "ordem das peças" ]
+    console.log(digitos)
+    console.log(posicaoAtual)
+    boxDigit1.textContent = ordemdaspeças[1]
+    boxDigit2.textContent = ordemdaspeças[2]
+    boxDigit4.textContent = ordemdaspeças[3]
+    boxDigit5.textContent = ordemdaspeças[5]
+    boxDigit6.textContent = ordemdaspeças[6]
+    boxDigit7.textContent = ordemdaspeças[7]
+    boxDigit8.textContent = ordemdaspeças[8]
+    boxDigit9.textContent = ordemdaspeças[9]
+    boxDigit10.textContent = ordemdaspeças[10]
+    boxDigit11.textContent = ordemdaspeças[11] 
+    boxDigit12.textContent = ordemdaspeças[12]
+    boxDigit13.textContent = ordemdaspeças[13] 
+    boxDigit14.textContent = ordemdaspeças[14]
+    boxDigit15.textContent = ordemdaspeças[15]
+    boxDigit16.textContent = ordemdaspeças[15]
+
+
+
+  
+  
+  
+  
+  functionmostrarpeçasdojogo ("1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16") {
+    Numerodaspeças= 1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16
+    imgpeçasdojogo.setAttribute("src", `./assets/${imgpeçasdojogo.foto}`)
+    titleNamedaspeças.textContent = peças.nome
+    const numerodaspeças = `${peças.numero}`.slice(1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16)
+    constjogodamemoria = jogo.find((imagemdaspeças) => p.numero === Number(numeroPeçasdojogo))
+    titleParty.textContent = peças.nome
+    divjogodamemoria.numerodaspeçasdojogo.appendChild(img,1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16)
+    divimagemdaspeças.appendChild(imagem.png)
+    divordemdaspeças.appemdChild(ordemdaspeças)
+  }
+  
+  function limparCandidato() {
+    divCandidate.removeChild(imgCandidate)
+    divCandidate.removeChild(titleNameCandidate)
+    divCandidate.removeChild(titleParty)
+  }
+  
+  function mostrarDigitos() {
+    if (voto === "branco") {
+      divDigits.removeAttribute("class", "white-vote")
+      divDigits.removeChild(message)
+      divDigits.appendChild(boxDigit1)
+      divDigits.appendChild(boxDigit2)
+      divDigits.appendChild(boxDigit3)
+      divDigits.appendChild(boxDigit4)
+      divDigits.appendChild(boxDigit5)
+    }
+  }
+  
+  function buttonDigitPressed(buttonNumber) {
+    if (posicaoAtual >= 0 && posicaoAtual < 5) {
+      digitos[posicaoAtual] = buttonNumber
+      posicaoAtual += 1
+      atualizaDigitos()
+    }
+    // se a posicao atual for maior ou igual a 5 
+    // vai procurar o candidato na lista de candidatos e mostrar seus dados
+    if (posicaoAtual == 5) {
+      const numeroDigitado = digitos.join("")
+      voto = numeroDigitado
+      const candidatoExiste = candidatos.find((candidato) => candidato.numero == numeroDigitado)
+      if (!candidatoExiste) return
+      mostrarCandidato(candidatoExiste)
+  
+    }
+  }
+  
+  buttonNumber1.addEventListener("click", (event) => {
+    event.preventDefault()
+    buttonDigitPressed(1)
+  })
+  
+  buttonNumber2.addEventListener("click", (event) => {
+    event.preventDefault()
+    buttonDigitPressed(2)
+  })
+  
+  buttonNumber3.addEventListener("click", (event) => {
+    event.preventDefault()
+    buttonDigitPressed(3)
+  })
+  
+  buttonNumber4.addEventListener("click", (event) => {
+    event.preventDefault()
+    buttonDigitPressed(4)
+  })
+  
+  buttonNumber5.addEventListener("click", (event) => {
+    event.preventDefault()
+    buttonDigitPressed(5)
+  })
+  
+  buttonNumber6.addEventListener("click", (event) => {
+    event.preventDefault()
+    buttonDigitPressed(6)
+  })
+  
+  buttonNumber7.addEventListener("click", (event) => {
+    event.preventDefault()
+    buttonDigitPressed(7)
+  })
+  
+  buttonNumber8.addEventListener("click", (event) => {
+    event.preventDefault()
+    buttonDigitPressed(8)
+  })
+  
+  buttonNumber9.addEventListener("click", (event) => {
+    event.preventDefault()
+    buttonDigitPressed(9)
+  })
+  
+  buttonNumber0.addEventListener("click", (event) => {
+    event.preventDefault()
+    buttonDigitPressed(0)
+  })
+  
+  // Para cada número, preciso saber qual a posição atual do digito e preencher com ele
+  
+  
+  // Se o comando for branco, todos os digitos ficam em branco
+  // remover as divs dos digitos e mostrar o h1 da mensagem
+  buttonCommandWhite.addEventListener("click", (event) => {
+    event.preventDefault()
+    if (voto === "branco") return
+  
+    divDigits.removeChild(boxDigit1)
+    divDigits.removeChild(boxDigit2)
+    divDigits.removeChild(boxDigit3)
+    divDigits.removeChild(boxDigit4)
+    divDigits.removeChild(boxDigit5)
+  
+    divDigits.setAttribute("class", "white-vote")
+    divDigits.appendChild(message)
+  
+    message.textContent = "VOTO EM BRANCO"
+    voto = "branco"
+  })
+  
+  // Se o comando for corrigir, apaga o digito atual
+  buttonCommandErase.addEventListener("click", (event) => {
+    event.preventDefault()
+    // caso o voto esteja em branco, então só adiciona os campos dos digitos na tela novamente
+    mostrarDigitos()
+    console.log(voto)
+    if (voto !== "branco" && voto !== "nulo") {
+      limparCandidato()
+    }
+    voto = undefined
+  
+    limparDigitos()
+    atualizaDigitos()
+  })
+  // Se o comando for confirmar, faz as seguintes validações:
+  // Se foram preenchidos todos os números, vai validar se o candidato é válido
+  // Se o candidato é valido, vai mostrar a foto, o nome e o partido do candidato
+  // Se o candidato não é valido, não mostra a foto, o nome e partido
+  // Pressionando uma segunda vez o confirma, registra o voto
+  
+  buttonCommandConfirm.addEventListener("click", (event) => {
+    event.preventDefault()
+    if (!voto) return
+    console.log(typeof voto)
+    const posicaoCandidato = candidatos.findIndex((candidato) => String(candidato.numero) === voto)
+    const posicaoCandidatoNulo = candidatos.findIndex((candidato) => candidato.numero === "nulo")
+    if (posicaoCandidato === -1) {
+      candidatos[posicaoCandidatoNulo].votos += 1
+    } else {
+      candidatos[posicaoCandidato].votos += 1
+      if (voto !== "branco") {
+        limparCandidato()
+      } else {
+        mostrarDigitos()
+      }
+    }
+  
+    voto = undefined
+    limparDigitos()
+    atualizaDigitos()
+    console.log(candidatos)
+  })
+  
+  
+
+
+
+  
+  
+  
